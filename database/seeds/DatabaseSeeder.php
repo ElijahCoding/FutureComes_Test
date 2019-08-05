9ac326fe057ec153c09a5cf5b59861d400b20553
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-05 20:42:56 +0300

    db seeder built, building views

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        factory('App\User', 100)->create();
+        Artisan::call('migrate:fresh');
+
+        factory('App\Models\User')->create([
+            'name' => 'elijah',
+            'email' => 'elijah@gmail.com',
+            'password' => bcrypt('hellojava')
+        ]);
+
+        factory('App\Models\User', 20)->create();
+
+        factory('App\Models\Article', 5)->create(['user_id' => 1]);
+        factory('App\Models\Article', 10)->create(['user_id' => 2]);
+        factory('App\Models\Article', 15)->create(['user_id' => 3]);
+        factory('App\Models\Article', 20)->create(['user_id' => 4]);
+
+        factory('App\Models\Comment', 10)->create(['article_id' => 2, 'user_id' => 1]);
+        factory('App\Models\Comment', 15)->create(['article_id' => 3, 'user_id' => 2]);
+        factory('App\Models\Comment', 20)->create(['article_id' => 4, 'user_id' => 3]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
-
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', 'HomeController@index')->name('home');
